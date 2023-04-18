a5235d53de24778af1a5de05d982a90e03236bf7
vlazoryk <v.lazoryk@chnu.edu.ua>
2023-04-18 12:07:06 +0300

    lab7

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,36 @@
-public class Main {
+public
+class Main {
 
     public static void main(String[] args) {
-        System.out.println(" Java lab 5");
+        System.out.println(" Java lab 7 ");
+        ThreadStarter.main(args);
 	// write your code here
     }
 }
+
+//public
+  class ThreadStarter {
+    public static void main(String[] args) {
+        NamedRunnable nr = new NamedRunnable();
+        Thread one = new Thread(nr);
+        Thread two = new Thread(nr);
+        Thread three = new Thread(nr);
+
+        one.setName("Перший");
+        two.setName("Другий");
+        three.setName("Третій");
+
+        one.start();
+        two.start();
+        three.start();
+    }
+}
+
+class NamedRunnable implements Runnable {
+    public void run() {
+        System.out.println("Завантажений " + Thread.currentThread().getName());
+        System.out.println("Закінченний  "+ Thread.currentThread().getName());
+    }
+}
+
+
